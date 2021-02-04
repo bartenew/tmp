69d3b90ddf8ec7302d17c5f1e87535ff80ce56a4
Arseniy A <arseniyandru@gmail.com>
2021-02-03 22:00:06 -0800

    docs: added javascript docs and comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import {caesarShift} from './caesar-shift';
 import assert = require('assert');
 
+/**
+ * Encrypt each word of a sentence, then encrypt each pair of resulting words until left with a single string.
+ */
 export function encryptSentence(input: string, encKey: number): string {
   console.log(`You entered: "${input}" with encryption key "${encKey}"`);
   assert(
@@ -12,26 +15,30 @@ export function encryptSentence(input: string, encKey: number): string {
 
 function _encryptSentence(input: string, encryptionKey: number) {
   const words = input.split(' ');
-
   const encryptedQueue = words.map(word => encrypt(word, encryptionKey));
 
+  // take and encrypt pairs from the queue until only 1 pair left
   while (encryptedQueue.length > 2) {
     const pair = popPair(encryptedQueue);
     encryptedQueue.push(encrypt(`${pair[0]}${pair[1]}`, encryptionKey));
   }
+  // encrypt last pair and return single resulting string
   return encrypt(
     `${encryptedQueue.pop()}${encryptedQueue.pop()}`,
     encryptionKey
   );
 }
 
-export function encrypt(token: string, key: number): string {
-  // return strongEncryption(token, key);
-  return caesarShift(token, key);
+/**
+ * Encrypt text with a given key.
+ */
+export function encrypt(str: string, key: number): string {
+  // return strongEncryption(str, key);
+  return caesarShift(str, key);
 }
 
-export function decrypt(token: string, key: number): string {
-  return caesarShift(token, -key);
+export function decrypt(str: string, key: number): string {
+  return caesarShift(str, -key);
 }
 
 function popPair(queue: string[]): [string, string] {
